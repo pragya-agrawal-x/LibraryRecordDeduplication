b586e826752e230d6b38bee964357e744f0c96c1
pragya-agrawal-x <pragya.agrwal@gmail.com>
2020-07-26 16:57:20 +0530

    added spark scala scripts in comments

--- analysis/name_dedup.py
+++ analysis/name_dedup.py
@@ -3,9 +3,46 @@ import pandas as pd
 # File location to be changed
 file_path = "DedupPhoneNum.csv"
 
+
+def extract_digits(input):
+    return ''.join(i for i in input if i.isdigit())
+
 '''
 Original Dataframe
 '''
 df = pd.read_csv(file_path, dtype=str)
-empty_phone_df = df[df['Aadhaar/ ID no.'].isna()]
-print(empty_phone_df)
+# df['aadhar'] = df['Aadhaar/ ID no.'].reset_index(name='co').applymap(extract_digits)
+print(type(df['Aadhaar/ ID no.']))
+# df['aadhar'] = extract_digits(df['Aadhaar/ ID no.'].str)
+# df = df.assign(aadhar= lambda x: ''.join(i for i in x['Aadhaar/ ID no.'] if i.isdigit()))
+# empty_phone_df['clean aadhar'] = pd.DataFrame.ap(df['Aadhaar/ ID no.'].values)
+# print(df)
+
+
+
+'''
+Scala code for aadhar and member ID detection:
+def extractDigit(ugly: String): String = {
+    val filtered = ugly.filter(_.isDigit)
+    
+    if(filtered.size == 12) filtered else ""    
+}
+
+val temp= udf{ (c1: String, c2:String) =>
+    val c1Extracted = extractDigit(c1)
+    val c2Extracted = extractDigit(c2)
+    if (c1Extracted == "") c2Extracted else c1Extracted
+}
+
+val mem= udf{ (c1: String, c2:String) =>
+    val c1Extracted = extractDigit(c1)
+    val c2Extracted = extractDigit(c2)
+    if (c1Extracted == "") c2Extracted else c1Extracted
+}
+
+val dfx = df.withColumn("clean", temp(col("AadhaarID"), col("Address"), col("Parent_Guardian")))
+dfx.groupBy("clean").count.sort(desc("count")).show
+d2.withColumn("mem", regexp_extract($"Parent_Guardian", "[mM][dD][ -]\\d{1,4}", 0)).("mem1", substring(col("mem"), 4, 10)).show(500)
+
+'''
+
